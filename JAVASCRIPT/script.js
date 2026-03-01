36f19d6bcbbc0a9f28b14488158424566c729bee
Teju <tejaswinimadhuranaidu@gmail.com>
2026-03-01 22:31:31 +0530

    JS variables,Datatypes completed

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,60 @@
+console.log("hello world");
+var a;
+a=10;
+let b=20
+const c=30;
+console.log(a,b,c);
+//Data types
+var num=10;
+var str="teju"
+var bool=true;
+var un;
+var nu=null;
+var big=123456789
+var symbol=Symbol("li");
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof nu);
+console.log(typeof big);
+console.log(typeof symbol);
+var arr=[1,2,3,4,5]
+console.log(arr);
+console.log(typeof arr);
+var obj={
+    name:"teju",
+    dept:"Ds"
+  }
+console.log(obj);
+console.log(typeof obj);
+var a=10;
+var d="2";
+console.log(a+d);
+console.log(d-a);
+console.log(a*d);
+console.log(a/d);
+console.log(a**d);
+console.log(a%d);
+console.log(a-d);
+//relational operators
+var e=10;
+var f=20;
+console.log(e==f);
+console.log(e===f);
+console.log(e!=f);
+console.log(e!==f);
+console.log(e>f);
+console.log(e<f);
+console.log(e>=f);
+console.log(e<=f);
+
+
+//logical operators
+var g=true;
+var h=false;
+
+console.log(g&&h);
+console.log(g||h);
+console.log(!g);
+
