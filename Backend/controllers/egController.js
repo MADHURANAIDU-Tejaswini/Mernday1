ce3242634bb82465a6ab7ad5cbd27a93b56641ae
Teju <tejaswinimadhuranaidu@gmail.com>
2026-03-09 16:12:06 +0530

    server creation ,MongoDB connection,Routing and controller Created

---
+++ Backend/controllers/egController.js
@@ -0,0 +1,16 @@
+exports.getController = (req,res)=>{
+        res.send("Get Route is Working");
+
+}
+exports.postController = (req,res)=>{
+        res.send("post Route is working");
+
+}
+exports.putController = (req,res)=>{
+        res.send("put Route is working");
+
+}
+exports.deleteController = (req,res)=>{
+        res.send("delete Route is working");
+
+}
